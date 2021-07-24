499a85888ce08cb5f5835c9816f8ec088f7e37bf
Manish Ranjan <manishranjan2606@gmail.com>
2021-07-24 23:53:39 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // console.log("This is my index js file");
 
 function requestXml(source,name) {
-  let apiKey = 'use_your_own_apikey';
+  let apiKey = '376ebc2ea366462db809cb0be1828359';
 
   // Grab the news container
   let newsAccordion = document.getElementById('newsAccordion');
